7bac45b31a449cef065fc07dfab78bc244fd8496
UrbanUsling <115050797+UrbanUsling@users.noreply.github.com>
2022-10-06 02:38:01 +0200

    Add files via upload

---
+++ src/Inlämning1/HuvudprogramVäxt.java
@@ -0,0 +1,46 @@
+package Inlämning1;
+import javax.swing.*;
+import java.util.*;
+
+public class HuvudprogramVäxt {//huvudklass
+    public HuvudprogramVäxt(){//konstruktor
+        Map<String, String> plants= new HashMap<>();
+        //hashmap skapas. Praktisk lista för att spara namn och dos (string och string) för enkel utskrift
+        Växt putte = new Palm("Putte", 1);
+        plants.put(putte.getNamn(), putte.getDos());
+        Växt igge = new Kaktus("Igge",  0.2);
+        plants.put(igge.getNamn(), igge.getDos());
+        Växt laura = new Palm("Laura", 5);
+        plants.put(laura.getNamn(), laura.getDos());
+        Växt meatloaf = new KöttVäxt("Meatloaf", 0.7);
+        plants.put(meatloaf.getNamn(), meatloaf.getDos());
+        /*
+        Objekt skapas av de fyra plantorna. Fler kan läggas till. De appliceras i hashmap också.
+        Alla objekten ÄR växter OCH en av subklasserna. Kallas Casting. Växt bestämmer vilka metoder vi kan anropa.
+        Sublklassen ger oss metodkroppen och konstruktor. Vi borde göra Palm p = new Palm(), MEN jag ville
+        visa hur arv fungerar. Allt vi är ute efter är getNamn och getDos och superklassen har dem abstrakt.
+        Jag kan inte använda metoden getLängd t ex för dessa objekt då den ej finns
+        i superklassen.
+         */
+        String namn = (JOptionPane.showInputDialog(null,"Vilken växt ska vattnas?",
+                "Val av planta", JOptionPane.QUESTION_MESSAGE));
+        //dialogruta som sparar växtnamnet man vill ha i string namn
+        if(namn == null)
+            System.exit(0);//Om inget skrivs in så avslutas programmet
+        String dos = plants.get(namn.toLowerCase());
+        //Ny variabel dos kontrollerar ifall det finns en planta i hashmap som stämmer överens med inputet (lowercase)
+        if(dos == null){
+            JOptionPane.showMessageDialog(null, "Denna växt är tyvärr ej här", "Inget vatten",
+                    JOptionPane.PLAIN_MESSAGE, new ImageIcon("src\\Inlämning1\\bilder\\plantor.jpg"));
+            //Om man får null betyder det att inget index i hashmap stämde överens. Den plantan finns inte. Rutan informerar
+        }
+        else {
+            JOptionPane.showMessageDialog(null, namn + " ska ha " + dos, "Bevattning",
+                    JOptionPane.PLAIN_MESSAGE, new ImageIcon("src\\Inlämning1\\bilder\\" + namn + ".png"));
+        }//Om den plantan finns på hotellet får man meddelande om vattenmängden och vattentypen den ska ha
+    }
+    public static void main(String[] args){//main
+        HuvudprogramVäxt ph = new HuvudprogramVäxt();//ny instance av huvudkonstruktorn körs
+
+    }
+}
